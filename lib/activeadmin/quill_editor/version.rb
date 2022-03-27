597cae7adb7635d3f2bc0d5d970c48a81a221222
Mattia Roccoberton <mattia.roccoberton@treatwell.com>
2022-03-27 11:07:06 +0200

    Test the Quill JS presence and version

--- lib/activeadmin/quill_editor/version.rb
+++ lib/activeadmin/quill_editor/version.rb
@@ -3,5 +3,6 @@
 module ActiveAdmin
   module QuillEditor
     VERSION = '0.3.4'
+    QUILL_VERSION = '1.3.7'
   end
 end
---
+++ spec/system/quill_js_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Quill JS', type: :system do
+  it 'has a Javascript function defined and returns the version', :aggregate_failures do
+    visit '/admin/posts'
+
+    expect(page.evaluate_script('typeof Quill')).to eq 'function'
+    expect(page.evaluate_script('Quill.version')).to eq(ActiveAdmin::QuillEditor::QUILL_VERSION)
+  end
+end
