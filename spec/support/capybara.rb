57e151aa038817716408765777874e8947c29c30
Mattia Roccoberton <mat@blocknot.es>
2025-03-16 10:48:10 +0100

    test: Fix ProcessTimeoutError when using Docker
    - Error details:
    
    Ferrum::ProcessTimeoutError:
      Browser did not produce websocket url within 10 seconds, try to increase `:process_timeout`
    
    - Reference issue: https://github.com/rubycdp/cuprite/issues/180

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,12 +1,28 @@
 # frozen_string_literal: true
 
-Capybara.server = :puma
-Capybara.default_driver = Capybara.javascript_driver = :cuprite
+require 'capybara/cuprite'
+
+Capybara.register_driver(:capybara_cuprite) do |app|
+  browser_options = {}.tap do |opts|
+    opts['no-sandbox'] = nil if ENV['DEVEL']
+  end
+
+  Capybara::Cuprite::Driver.new(
+    app,
+    window_size: [1600, 1280],
+    browser_options: browser_options,
+    process_timeout: 20,
+    timeout: 20,
+    inspector: true,
+    headless: !ENV['CUPRITE_HEADLESS'].in?(%w[n 0 no false])
+  )
+end
+
+# Capybara.server = :puma
+Capybara.default_driver = Capybara.javascript_driver = :capybara_cuprite
 
 RSpec.configure do |config|
-  # Make sure this hook runs before others
   config.prepend_before(:each, type: :system) do
-    # Use JS driver always
     driven_by Capybara.javascript_driver
   end
 end
